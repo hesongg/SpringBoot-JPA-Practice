87bfcfcae4b86491d5f2c5cfd44e30a8ccec3ea4
hesung <hsjo6864@gmail.com>
2022-09-23 14:41:02 +0900

    2022.09.03 SpringBoot-JPA Practice Project - 프로젝트 환경설정  - yml 파일 추가 기본 세팅, 개발용 옵션 설정  - 추가 라이브러리로 devtools - 템플릿 서버 재시작없이사용가능,   쿼리 파라미터 찍어주는 라이브러리 추가 등 - JPA , h2 환경 설정 테스트 수행

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Commit
+    void testMember() {
+        //given
+        Member member = new Member();
+        member.setUserName("memberA");
+
+        //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+
+        //then
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+
+    }
+}
